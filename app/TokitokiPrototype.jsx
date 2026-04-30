c4a8178cc85c73ca2ab94683d4a4681d9d4d6110
idamniedam <idamniedam@gmail.com>
2026-04-30 17:23:29 +0200

    l

--- app/TokitokiPrototype.jsx
+++ app/TokitokiPrototype.jsx
@@ -969,7 +969,32 @@ export default function TokitokiPrototype() {
         goToMobileCart={goToMobileCart}
       />
       <CookieBanner />
-    </div>
+
+<script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "LocalBusiness",
+      name: "TOKITOKI",
+      legalName: "Kruszywa Polska Sp. z o.o.",
+      telephone: "+48692988692",
+      email: "biuro@kruszywapolska.pl",
+      url: "https://tokitoki.pl",
+      image: "https://tokitoki.pl/hero-taczki-rzeszow.png",
+      address: {
+        "@type": "PostalAddress",
+        streetAddress: "ul. Porąbki 13",
+        postalCode: "35-317",
+        addressLocality: "Rzeszów",
+        addressCountry: "PL",
+      },
+      areaServed: ["Rzeszów", "okolice Rzeszowa"],
+      priceRange: "$$",
+    }),
+  }}
+/>
+</div>
   );
 }
 
