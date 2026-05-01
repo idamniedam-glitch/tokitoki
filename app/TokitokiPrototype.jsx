aebf473ae0a18a60acd6858e6dd631a1226a9959
idamniedam <idamniedam@gmail.com>
2026-05-01 06:57:00 +0200

    cookie

--- app/TokitokiPrototype.jsx
+++ app/TokitokiPrototype.jsx
@@ -969,32 +969,37 @@ export default function TokitokiPrototype() {
         goToMobileCart={goToMobileCart}
       />
       <CookieBanner />
+      <LocalBusinessSchema />
+    </div>
+  );
+}
 
-<script
-  type="application/ld+json"
-  dangerouslySetInnerHTML={{
-    __html: JSON.stringify({
-      "@context": "https://schema.org",
-      "@type": "LocalBusiness",
-      name: "TOKITOKI",
-      legalName: "Kruszywa Polska Sp. z o.o.",
-      telephone: "+48692988692",
-      email: "biuro@kruszywapolska.pl",
-      url: "https://tokitoki.pl",
-      image: "https://tokitoki.pl/hero-taczki-rzeszow.png",
-      address: {
-        "@type": "PostalAddress",
-        streetAddress: "ul. Porąbki 13",
-        postalCode: "35-317",
-        addressLocality: "Rzeszów",
-        addressCountry: "PL",
-      },
-      areaServed: ["Rzeszów", "okolice Rzeszowa"],
-      priceRange: "$$",
-    }),
-  }}
-/>
-</div>
+function LocalBusinessSchema() {
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    name: "TOKITOKI",
+    legalName: "Kruszywa Polska Sp. z o.o.",
+    telephone: "+48692988692",
+    email: "biuro@kruszywapolska.pl",
+    url: "https://tokitoki.pl",
+    image: "https://tokitoki.pl/hero-taczki-rzeszow.png",
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "ul. Porąbki 13",
+      postalCode: "35-317",
+      addressLocality: "Rzeszów",
+      addressCountry: "PL",
+    },
+    areaServed: ["Rzeszów", "okolice Rzeszowa"],
+    priceRange: "$$",
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+    />
   );
 }
 
