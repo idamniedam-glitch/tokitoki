3ceb6fa45309cb8e3be5313b0966c2314771b2c5
idamniedam <idamniedam@gmail.com>
2026-05-02 14:27:44 +0200

    add old url redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/kruszywa",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/oferta",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/kontakt",
+        destination: "/#kontakt",
+        permanent: true,
+      },
+      {
+        source: "/:slug",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
