fee294de886298239ba5e4d11918a3fbb7bee4e9
idamniedam <idamniedam@gmail.com>
2026-05-04 10:01:26 +0200

    n2

--- app/TokitokiPrototype.jsx
+++ app/TokitokiPrototype.jsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 import Header from "./components/Header";
 import CookieBanner from "./components/CookieBanner";
+import LocalBusinessSchema from "./components/LocalBusinessSchema";
 import {
   ShoppingCart,
   Phone,
@@ -976,34 +977,7 @@ export default function TokitokiPrototype() {
   );
 }
 
-function LocalBusinessSchema() {
-  const schema = {
-    "@context": "https://schema.org",
-    "@type": "LocalBusiness",
-    name: "TOKITOKI",
-    legalName: "Kruszywa Polska Sp. z o.o.",
-    telephone: "+48692988692",
-    email: "biuro@kruszywapolska.pl",
-    url: "https://tokitoki.pl",
-    image: "https://tokitoki.pl/hero-taczki-rzeszow.png",
-    address: {
-      "@type": "PostalAddress",
-      streetAddress: "ul. Porąbki 13",
-      postalCode: "35-317",
-      addressLocality: "Rzeszów",
-      addressCountry: "PL",
-    },
-    areaServed: ["Rzeszów", "okolice Rzeszowa"],
-    priceRange: "$$",
-  };
 
-  return (
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
-    />
-  );
-}
 
 
 
