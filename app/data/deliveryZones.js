0f2c38315485b20a6726644f0c5021d2641a3101
idamniedam <idamniedam@gmail.com>
2026-05-04 10:37:54 +0200

    123

---
+++ app/data/deliveryZones.js
@@ -0,0 +1,14 @@
+export const deliveryZones = [
+  {
+    id: "rzeszow",
+    name: "Rzeszów",
+    price: 100,
+    note: "stała cena dostawy na terenie miasta",
+  },
+  {
+    id: "area",
+    name: "Okolice Rzeszowa",
+    price: null,
+    note: "wycena indywidualna po podaniu adresu",
+  },
+];
---
+++ app/data/products.js
@@ -0,0 +1,191 @@
+export const products = [
+  {
+    id: "piasek-do-piaskownic",
+    type: "build",
+    subcategory: "Piasek",
+    name: "Piasek do piaskownic",
+    priceNet: 80,
+    unit: "t",
+    description:
+      "Jasny, drobny piasek do piaskownic, placów zabaw i przestrzeni rekreacyjnych przy domu. Dostawa na terenie Rzeszowa i okolic.",
+    image: "/produkty/piasek-do-piaskownic.jpg",
+  },
+  {
+    id: "piasek-zasypowy",
+    type: "build",
+    subcategory: "Piasek",
+    name: "Piasek zasypowy",
+    priceNet: 70,
+    unit: "t",
+    description:
+      "Piasek do zasypywania wykopów, wyrównywania terenu, fundamentów i prac ziemnych. Dostępny z transportem w Rzeszowie i okolicach.",
+    image: "/produkty/piasek-zasypowy.jpg",
+  },
+  {
+    id: "piasek-plukany-0-2",
+    type: "build",
+    subcategory: "Piasek",
+    name: "Piasek płukany 0–2 mm",
+    priceNet: 80,
+    unit: "t",
+    description:
+      "Czysty piasek płukany do betonu, zapraw, wylewek i prac budowlanych. Popularny wybór na budowę i remont.",
+    image: "/produkty/piasek-plukany-0-2.jpg",
+  },
+  {
+    id: "piasek-do-tynkow-0-1",
+    type: "build",
+    subcategory: "Piasek",
+    name: "Piasek do tynków 0–1 mm",
+    priceNet: 85,
+    unit: "t",
+    description:
+      "Drobny piasek do tynków, zapraw murarskich i prac wykończeniowych wymagających równej, drobnej frakcji.",
+    image: "/produkty/piasek-do-tynkow-0-1.jpg",
+  },
+  {
+    id: "zwir-2-8",
+    type: "build",
+    subcategory: "Żwir",
+    name: "Żwir 2–8 mm",
+    priceNet: 95,
+    unit: "t",
+    description:
+      "Drobny żwir do betonu, drenaży, podsypek oraz prac ogrodowo-budowlanych. Dostawa Rzeszów i okolice.",
+    image: "/produkty/zwir-2-8.jpg",
+  },
+  {
+    id: "zwir-8-16",
+    type: "build",
+    subcategory: "Żwir",
+    name: "Żwir 8–16 mm",
+    priceNet: 100,
+    unit: "t",
+    description:
+      "Uniwersalny żwir do betonu, drenażu, opasek wokół budynków, podjazdów i prac przy domu.",
+    image: "/produkty/zwir-8-16.jpg",
+  },
+  {
+    id: "zwir-16-32",
+    type: "build",
+    subcategory: "Żwir",
+    name: "Żwir 16–32 mm",
+    priceNet: 105,
+    unit: "t",
+    description:
+      "Grubszy żwir do drenaży, odwodnień, warstw filtracyjnych i prac ziemnych wokół budynków.",
+    image: "/produkty/zwir-16-32.jpg",
+  },
+  {
+    id: "grys-2-8",
+    type: "build",
+    subcategory: "Grys",
+    name: "Grys 2–8 mm",
+    priceNet: 110,
+    unit: "t",
+    description:
+      "Drobny grys do utwardzeń, podjazdów, ścieżek, drenażu i prac wykończeniowych. Kruszywo z dostawą w Rzeszowie.",
+    image: "/produkty/grys-2-8.jpg",
+  },
+  {
+    id: "grys-8-16",
+    type: "build",
+    subcategory: "Grys",
+    name: "Grys 8–16 mm",
+    priceNet: 115,
+    unit: "t",
+    description:
+      "Popularna frakcja grysu do podjazdów, ścieżek, opasek przy budynkach, utwardzeń i prac ogrodowych.",
+    image: "/produkty/grys-8-16.jpg",
+  },
+  {
+    id: "tluczen-32-63",
+    type: "build",
+    subcategory: "Tłuczeń i kliniec",
+    name: "Tłuczeń 32–63 mm",
+    priceNet: 95,
+    unit: "t",
+    description:
+      "Grube kruszywo do mocnych podbudów, dróg dojazdowych, placów, parkingów i utwardzania terenu.",
+    image: "/produkty/tluczen-32-63.jpg",
+  },
+  {
+    id: "kliniec-4-32",
+    type: "build",
+    subcategory: "Tłuczeń i kliniec",
+    name: "Kliniec 4–32 mm",
+    priceNet: 95,
+    unit: "t",
+    description:
+      "Kliniec do podjazdów, podbudów, wyrównywania terenu i zagęszczania nawierzchni pod dalsze prace.",
+    image: "/produkty/kliniec-4-32.jpg",
+  },
+  {
+    id: "kruszywo-niesortowane-0-32",
+    type: "build",
+    subcategory: "Tłuczeń i kliniec",
+    name: "Kruszywo niesortowane 0–32 mm",
+    priceNet: 85,
+    unit: "t",
+    description:
+      "Mieszanka drobnej i grubszej frakcji do stabilnych podbudów, utwardzeń, wyrównania terenu i prac drogowych.",
+    image: "/produkty/kruszywo-0-32.jpg",
+  },
+  {
+    id: "kruszywo-niesortowane-0-63",
+    type: "build",
+    subcategory: "Tłuczeń i kliniec",
+    name: "Kruszywo niesortowane 0–63 mm",
+    priceNet: 85,
+    unit: "t",
+    description:
+      "Grubsza mieszanka do mocnych podbudów, utwardzania dróg, placów, parkingów i warstw nośnych.",
+    image: "/produkty/kruszywo-0-63.jpg",
+  },
+  {
+    id: "pospolka",
+    type: "build",
+    subcategory: "Pospółka",
+    name: "Pospółka żwirowo-piaskowa",
+    priceNet: 75,
+    unit: "t",
+    description:
+      "Naturalna mieszanka piasku i żwiru do podbudów, zasypek, wyrównywania terenu, fundamentów i prac drogowych.",
+    image: "/produkty/pospolka.jpg",
+  },
+  {
+    id: "przekrusz-betonowy-0-63",
+    type: "build",
+    subcategory: "Przekrusz betonowy",
+    name: "Przekrusz betonowy 0–63 mm",
+    priceNet: 60,
+    unit: "t",
+    description:
+      "Ekonomiczne kruszywo z recyklingu do utwardzania dróg, placów, podjazdów, dojazdów i podbudów.",
+    image: "/produkty/przekrusz-betonowy-0-63.jpg",
+  },
+  {
+    id: "grys-bialy",
+    type: "decor",
+    subcategory: "Grys ozdobny",
+    name: "Grys biały dekoracyjny",
+    priceNet: 420,
+    unit: "t",
+    description:
+      "Do ogrodów, rabat, opasek wokół domu, dekoracji i nowoczesnych aranżacji terenu.",
+    image:
+      "https://images.unsplash.com/photo-1622467827417-bbe2237067a9?q=80&w=900&auto=format&fit=crop",
+  },
+  {
+    id: "otoczak-szary",
+    type: "decor",
+    subcategory: "Otoczak",
+    name: "Otoczak szary",
+    priceNet: 390,
+    unit: "t",
+    description:
+      "Naturalny kamień ozdobny do ogrodu, rabat, oczek wodnych i opasek przy elewacji.",
+    image:
+      "https://images.unsplash.com/photo-1616047006789-b7af5afb8c20?q=80&w=900&auto=format&fit=crop",
+  },
+];
